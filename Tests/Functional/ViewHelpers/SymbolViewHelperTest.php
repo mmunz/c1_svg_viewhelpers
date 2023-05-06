ad6b263bdc80d8ca9b58358267ef75cf137b57e1
Manuel Munz <munz@comuno.net>
2023-05-06 11:47:32 +0200

    Prepare for testing-framework 7

--- Tests/Functional/ViewHelpers/SymbolViewHelperTest.php
+++ Tests/Functional/ViewHelpers/SymbolViewHelperTest.php
@@ -16,13 +16,13 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 class SymbolViewHelperTest extends FunctionalTestCase
 {
     protected array $testExtensionsToLoad = [
-        '../../Tests/Fixtures/Extensions/c1_svg_viewhelpers_test',
-        '../../../c1_svg_viewhelpers',
+        'typo3conf/ext/c1_svg_viewhelpers/Tests/Fixtures/Extensions/c1_svg_viewhelpers_test',
+        'typo3conf/ext/c1_svg_viewhelpers',
     ];
 
     protected array $defaultArguments = [
-            'id' => 1,
-        ];
+        'id' => 1,
+    ];
 
     protected $backupGlobals = true;
 
